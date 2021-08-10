c6960ea1b729238fdb2c231bf876c54f63093e0e
Leonardo da Rocha Ferrari <1011492@UNIDAVI.LOCAL>
2021-08-09 21:49:44 -0300

    dale

--- calculadora.js
+++ calculadora.js
@@ -2,16 +2,29 @@
     function getCalculadora() {
         var oDiv = document.getElementById("container"),
             oForm = document.createElement('form'),
-            oTable = document.createElement('table'),
-            oTr = document.createElement('tr'),
-            oTd = document.createElement('td'),
-            oInput = document.createElement('input');
-            oQuebraLinha = document.createElement('br');
+            oTable = document.createElement('table');
 
-        oForm.setAttribute('method', 'POST');
+        oForm.setAttribute('method', 'POST')
 
         oTable.setAttribute('style', 'border: 1px solid black');
         oTable.setAttribute('class', 'tbGeral');
+
+
+        oTable.appendChild(getLinhaResultado());
+        oTable.appendChild(getLinhaReset());
+
+        oForm.appendChild(oTable);
+
+        oDiv.appendChild(oForm);
+        oDiv.appendChild(document.createTextNode("oi"))
+            
+    }
+
+    function getLinhaResultado() {
+        var oTr = document.createElement('tr'),
+        oTd = document.createElement('td'),
+        oInput = document.createElement('input');
+        oQuebraLinha = document.createElement('br');
 
         oTd.setAttribute('colspan', 4);
         oTd.setAttribute('class', 'colunaTab');
@@ -24,19 +37,13 @@
         oInput.setAttribute('disabled', true);
         oInput.appendChild(oQuebraLinha);
         
-        
         oTd.appendChild(oInput);
 
         oTr.appendChild(oTd);
 
-        oTable.appendChild(oTr);
-
-        oForm.appendChild(oTable);
-
-        oDiv.appendChild(oForm);
-        oDiv.appendChild(document.createTextNode("oi"))
-            
+        return oTr;
     }
+
 
     function pegaValor() {
         return 0;
